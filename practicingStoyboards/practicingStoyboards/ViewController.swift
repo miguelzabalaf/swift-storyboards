c04fac4e44a41d892f34c54998e3e3bfbdf49102
Miguel Zabala <zabalafmiguel@hotmail.com>
2024-11-09 00:45:54 -0500

    [COUNTER] - I practiced with some v/h stacks, buttons, constraints and interaction with my viewController

--- practicingStoyboards/practicingStoyboards/ViewController.swift
+++ practicingStoyboards/practicingStoyboards/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var decrementButton: UIButton!
+
+    var counter: Int = 0 {
+        didSet {
+            setValues()
+            validate(decrementButton)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setValues()
+        validate(decrementButton)
     }
-
-
+    
+    func setValues() -> Void {
+        self.counterLabel.text = String(counter)
+    }
+    
+    func validate(_ button: UIButton) {
+        if counter == 0 {
+            button.isEnabled = false
+        } else {
+            button.isEnabled = true
+        }
+    }
+    
+    @IBAction func incrementButtonDidPress() {
+        counter += 1
+    }
+    
+    @IBAction func decrementButtonDidPress() {
+        counter -= 1
+    }
+    
+    @IBAction func resetButtonDidPress() {
+        counter = 0
+    }
 }
 
