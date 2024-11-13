b33ae96801f8427081fd2bddff5b0c13b48dc77a
Miguel Zabala <zabalafmiguel@hotmail.com>
2024-11-12 22:10:05 -0500

    [UI NAVIGATION CONTROLLER] - practicing more about navigation with storyboards

---
+++ uiNavigationController/uiNavigationController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  uiNavigationController
+//
+//  Created by Miguel on 11/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
